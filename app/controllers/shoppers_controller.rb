0175cefd058c12c0c5e5ce48fe0fe32754eada33
lesliesage <leslie.sage@gmail.com>
2019-07-17 17:45:42 -0400

    Finished logic except for nested forms stuff

--- app/controllers/shoppers_controller.rb
+++ app/controllers/shoppers_controller.rb
@@ -1,8 +1,8 @@
 class ShoppersController < Sinatra::Base
   set :views, 'app/views/shoppers'
   set :method_override, true
-  
-  get '/shopper' do
+
+  get '/shoppers' do
     @shoppers = Shopper.all
     erb :index
   end
@@ -18,17 +18,42 @@ class ShoppersController < Sinatra::Base
 
   get '/shoppers/:id' do
     @shopper = Shopper.find(params[:id])
+    @items = Item.all
+    @items_hash = {}
+    Item.all.each do |item|
+      @items_hash[item.name] = 0
+    end
+    @shopper.orders.each do |order|
+      @items_hash[order.item.name] = order.quantity
+    end
     erb :show
+  end
+
+  post '/orders' do
+    @order = Order.find_or_create_by(shopper_id: params[], order_id: params[])
+  end
+
+  get '/shoppers/:id/confirmation' do
+    @shopper = Shopper.find(params[:id])
+    @items = Item.all
+    @items_hash = {}
+    Item.all.each do |item|
+      @items_hash[item.name] = 0
+    end
+    @shopper.orders.each do |order|
+      @items_hash[order.item.name] = order.quantity
+    end
+    erb :confirmation
   end
 
   get '/shoppers/:id/edit' do
     @shopper = Shopper.find(params[:id])
-    redirect '/shoppers'
+    erb :edit
   end
 
-  post '/shoppers/:id' do
+  patch '/shoppers/:id' do
     @shopper = Shopper.find(params[:id])
-    @shopper.update(params[:name])
+    @shopper.update(name: params[:name])
     redirect "/shoppers/#{@shopper.id}"
   end
 
